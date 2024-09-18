d3d261abd6bee7bd069ca164c0aa098d041819cc
jcampom01 <146666290+jcampom01@users.noreply.github.com>
2024-09-18 13:00:16 +0200

    Add files via upload

---
+++ src/main/java/jaime/indra/IndraProjectApplication.java
@@ -0,0 +1,13 @@
+package jaime.indra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaime/indra/IndraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jaime.indra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
