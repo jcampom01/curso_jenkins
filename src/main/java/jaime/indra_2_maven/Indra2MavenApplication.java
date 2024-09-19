db9c65a8cc2e8ecd967d238c54d52107a5b050fc
jcampom01 <146666290+jcampom01@users.noreply.github.com>
2024-09-19 10:22:38 +0200

    Add files via upload

---
+++ src/main/java/jaime/indra_2_maven/Indra2MavenApplication.java
@@ -0,0 +1,13 @@
+package jaime.indra_2_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Indra2MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Indra2MavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaime/indra_2_maven/Indra2MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package jaime.indra_2_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Indra2MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
