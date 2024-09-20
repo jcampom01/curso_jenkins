744ece65fdff77949174639cc560c16ba3329c0d
jcampom01 <146666290+jcampom01@users.noreply.github.com>
2024-09-20 12:02:54 +0200

    Add files via upload

---
+++ src/main/java/jaime/indra_integrador/IndraIntegradorApplication.java
@@ -0,0 +1,13 @@
+package jaime.indra_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaime/indra_integrador/IndraIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package jaime.indra_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
